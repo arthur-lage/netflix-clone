c59cc77d8e49c9f8d37aa2f31db712fc80c9550e
Arthur Lage <arthurlage2006@gmail.com>
2021-09-18 18:45:05 -0300

    cannot create account using the same email

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -19,16 +19,20 @@ export default ({ setIsLogged }) => {
       if(validate(email) === false){
         alert("Use um email válido")
       } else {
-        const newAccount = {
-          name,
-          email,
-          password
+        if(accounts.filter(account => account.email === email).length > 0){
+          alert("Uma conta com essas credenciais já foi criada")
+        } else {
+          const newAccount = {
+            name,
+            email,
+            password
+          }
+
+          setAccounts([...accounts, newAccount])
+
+          setIsLogged(true)
+          history.push("/")
         }
-
-        setAccounts([...accounts, newAccount])
-
-        setIsLogged(true)
-        history.push("/")
       }
     }
   };
